cac1c1921aa6b88248de3c5451a493d5e9fff9ac
Harlan Howe <hghowe@gmail.com>
2020-09-23 22:20:01 -0500

    fixed fonts, filled dots and dot ids, moved shape down.

--- BezierDemo/BezierDemo.js
+++ BezierDemo/BezierDemo.js
@@ -1,12 +1,12 @@
 var x =[100, 200, 200, 100];
-var y =[100, 100, 200, 200];
+var y =[150, 150, 250, 250];
 var whichDot = -1;
 
 function setup() 
 {
   createCanvas(800,800);
   textSize(16);
-  textFont("Arial");
+  
 }
 
 function draw() 
@@ -18,8 +18,14 @@ function drawCurve()
 {
   background(64);
   fill("white");
-  text("vertex(x[0],y[0]);",10,15);
-  text("bezierVertex(x[1],y[1],  x[2],y[2],  x[3],y[3]);",10,35);
+  textFont("Courier");
+  textSize(12);
+  text("beginShape();",10,15);
+  text("vertex(x[0],y[0]);",30,35);
+  text("bezierVertex(x[1],y[1],x[2],y[2],x[3],y[3]);",30,55);
+  text("endShape(CLOSE);",10,75);
+  textFont("Arial");
+  textSize(16);
   text("<-- the code at left is what is making the yellow shape.", 400,15);
   text("Drag the numbered dots around to see what they do!",400,35);
   text("(Imagine a line from 0 -> 1 and one from 3 ->2)", 400, 55);
@@ -31,11 +37,12 @@ function drawCurve()
     bezierVertex(x[1],y[1],  x[2],y[2],  x[3],y[3]);
   endShape(CLOSE);
   
-  noFill();
+  
   strokeWeight(1);
   for (var i=0; i<4; i++)
   {
       stroke(255,128+32*i,255-32*i);
+      fill(255,128+32*i,255-32*i);
       ellipse(x[i],y[i],10,10);
       text(""+i, x[i]+10, y[i]-10);
   }
